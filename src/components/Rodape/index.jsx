b609c66fbac80943a902500babf07e636bad994b
Dyegho <dyeghocunha@gmail.com>
2023-05-15 15:22:20 -0300

    Alteracao nos Filmes e no Dex

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,11 +1,12 @@
 import styles from './Rodape.module.scss';
 import bb8 from '../../assets/imagem/swCine_footer.png'
-import icone1 from '../../assets/imagem/icons/icons/1626612_star wars_chewbacca_wookie_chewie_rebel.png'
-import icone2 from '../../assets/imagem/icons/icons/1626616_sidious_palpatine_emperor_sith_star wars_empire_darth.png'
-import icone3 from '../../assets/imagem/icons/icons/1626615_jedi_star wars_sith_darth_vader_anakin_skywalker.png'
+import icone1 from '../../assets/imagem/icons/1.png'
+import icone2 from '../../assets/imagem/icons/9.png'
+import icone3 from '../../assets/imagem/icons/4.png'
 import instagram from './icones/instagram.svg'
 import facebook from './icones/facebook.svg'
 import github from './icones/github.svg'
+
 
 import React from 'react'
 
@@ -18,23 +19,26 @@ export default function Rodape() {
 
 
         <img src={bb8} alt="" />
+        <div className={styles.links}>
+          <div className={styles.linkContainer}>
 
-        <div className={styles.linkContainer}>
+            <LinkRodape icone={icone1} link={'./'}>Imagens dos Cards</LinkRodape>
+            <LinkRodape icone={icone2} link={'https://symbolicons.com/free#star-wars'}>Imagem dos Icones</LinkRodape>
+            <LinkRodape icone={icone3} link={'./'}>Galeria Start Wars</LinkRodape>
 
-          <LinkRodape icone={icone1} link={'./'}>Imagens dos Cards</LinkRodape>
-          <LinkRodape icone={icone2} link={'https://symbolicons.com/free#star-wars'}>Imagem dos Icones</LinkRodape>
-          <LinkRodape icone={icone3} link={'./'}>Galeria Start Wars</LinkRodape>
-          
 
+          </div>
+
+          <div className={styles.linkContainer}>
+
+            <LinkRodape icone={instagram} link={'./'}></LinkRodape>
+            <LinkRodape icone={facebook} link={'./'}></LinkRodape>
+            <LinkRodape icone={github} link={'./'}></LinkRodape>
+
+          </div>
+         
         </div>
-
-        <div className={styles.linkContainer}>
-
-          <LinkRodape icone={instagram} link={'./'}></LinkRodape>
-          <LinkRodape icone={facebook} link={'./'}></LinkRodape>
-          <LinkRodape icone={github} link={'./'}></LinkRodape>
-
-        </div>
+        <h3>Desenvolvido por Dyegho Cunha</h3>
 
       </div>
 
--- src/pages/Biografia/index.jsx
+++ src/pages/Biografia/index.jsx
@@ -5,6 +5,7 @@ import styles from './Biografia.module.scss';
 import listas from '../../json/lista.json'
 import React from 'react'
 import NaoEncontrada from '../NaoEncontrada';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 export default function Biografia() {
 
@@ -12,20 +13,26 @@ export default function Biografia() {
   const lista = listas.find((lista) => {
     return lista.id === Number(parametros.id)
   })
-  
-if(!lista){
-  return <NaoEncontrada/>
-}
+
+  if (!lista) {
+    return <NaoEncontrada />
+  }
   return (
     <>
       <Banner imagem="videos" />
       <Titulo>
         <h1>{lista.titulo}</h1>
       </Titulo>
-      <h1>{lista.descricao}</h1>
-      <h1>{lista.foto}</h1>
-      <h1>{lista.planeta}</h1>
-      
- </>
+      <div className={styles.biografiaContainer}>
+        <div className={styles.bioagrafia}>
+        <ReactMarkdown>
+          {lista.descricao}
+        </ReactMarkdown>
+        </div>  
+        <h1 className={styles.fotoPersonagem}>{lista.foto}</h1>
+        <h1>{lista.planeta}</h1>
+      </div>
+
+    </>
   )
 }
