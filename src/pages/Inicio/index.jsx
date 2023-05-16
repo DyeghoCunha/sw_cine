864fdd5b94a0984aee0ccd3cc877585a15df9d03
Dyegho <dyeghocunha@gmail.com>
2023-05-16 09:36:53 -0300

    final

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -8,29 +8,39 @@ import videos from '../../json/db.json'
 import styles from './Inicio.module.scss'
 
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Inicio() {
 
+  //Parte do codigo incluida para usar com o MY JSON SERVER
+  /*
+  const [videos, setVideos] = useState([]);
+   useEffect(() => {
+    fetch("https://my-json-server.typicode.com/dyeghocunha/swcine.api/videos")
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideos(dados)
+      })
+  }, []) */
 
   return (
     <>
-    
-    <Banner imagem="home"/>
-    <Titulo>
-      <h1>Um Lugar para Guardar seus vídeos e filmes!</h1>
-    </Titulo>
-    
-{/* Mapear o arquivo JSON  */}
 
-    <section className={styles.container}>
-      {videos.map((video)=>{
-       return  <Card {...video} key = {video.id}/>
-      })}
-    </section>
-{/* FIM DO MAPA */}
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um Lugar para Guardar seus vídeos e filmes!</h1>
+      </Titulo>
 
-    
+      {/* Mapear o arquivo JSON  */}
+
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
+      {/* FIM DO MAPA */}
+
+
     </>
   )
 }
